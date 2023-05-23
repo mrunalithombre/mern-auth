f3b653f1cd14b4efc3876cd807956180fb9fc77d
mrunali <mrunalithombre33@gmail.com>
2023-05-23 10:03:23 +0530

    23-5-23

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+
+exports.register = async (req, res) => {
+	try {
+		const { password } = req.body
+
+		const hashPass = await bcrypt.hash(password, 10)
+		const result = await User.create({
+			...req.body,
+			password: hashPass
+		})
+		res.json({
+			message: "User Register Success",
+			result,
+		})
+	} catch (error) {
+		res.json({ message: "Something Went Wrong", error })
+	}
+}
+
+exports.fetchUsers = async (req, res) => {
+	try {
+		const result = await User.find()
+		res.json({
+			message: "User fetch Success",
+			result
+		})
+	} catch (error) {
+		res.json({ message: "Something Went Wrong", error })
+	}
+}
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express")
-
+require("dotenv").config({ path: "./.env" })
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_URL)
 const app = express()
+
+app.use(express.json())//body parser
+
+app.use("/api/user", require("./routes/userRoutes"))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`http://localhost:${PORT}`))
+mongoose, mongoose.connection.once("open", () => {
+	console.log("DB CONNECTED")
+	app.listen(PORT, console.log(`http://localhost:${PORT}`))
+})
