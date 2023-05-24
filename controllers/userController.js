e5660052124957ce3a364c94a113b74ebd7c80cf
mrunali <mrunalithombre33@gmail.com>
2023-05-24 09:58:59 +0530

    24-5-23

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
 
 exports.register = async (req, res) => {
 	try {
-		const { password } = req.body
+		const { password, email } = req.body
+		const found = await User.findOne({ email })
+		if (found) {
+			return res.json({
+				message: "Email Already Exists"
+			})
+		}
+		const token = jwt.sign({ name: "kate" }, "SECRET_PASSWORD")
 
 		const hashPass = await bcrypt.hash(password, 10)
 		const result = await User.create({
@@ -21,6 +30,13 @@ exports.register = async (req, res) => {
 
 exports.fetchUsers = async (req, res) => {
 	try {
+		const token = req.headers.authorization
+		if (!token) {
+			return res.json({ message: "Provide Token" })
+		}
+
+		jwt.verify(token, process.env.JWT_KEY)
+
 		const result = await User.find()
 		res.json({
 			message: "User fetch Success",
@@ -30,3 +46,35 @@ exports.fetchUsers = async (req, res) => {
 		res.json({ message: "Something Went Wrong", error })
 	}
 }
+
+exports.login = async (req, res) => {
+	try {
+		//email exist
+		const { email, password } = req.body
+		const result = await User.findOne({ email })
+
+		if (!result) {
+			return res.json({ message: "Email is not registered with us" })
+		}
+		const match = await bcrypt.compare(password, result.password)
+		if (!match) {
+			return res.json({ message: "password do not match" })
+		}
+		const token = jwt.json({ name: "kate" }, process.env.JWT_KEY)
+		res.json({ message: "login success", token })
+
+	} catch (error) {
+		res.json({ message: "Something Went Wrong" + error })
+	}
+}
+
+exports.destroy = async (req, res) => {
+	try {
+		await User.deleteMany()
+		res.json({ message: "User Destroy Success" })
+
+	} catch (error) {
+		res.json({ message: "Something Went Wrong" + error })
+	}
+}
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
-const { fetchUsers, register } = require("../controllers/userController")
+const { fetchUsers, register, login, destroy } = require("../controllers/userController")
 
 const router = require("express").Router()
 
 router
 	.get("/", fetchUsers)
 	.post("/register", register)
+	.post("/login", login)
+	.delete("/destroy", destroy)
 
-module.exports = router
+module.exports = router		
